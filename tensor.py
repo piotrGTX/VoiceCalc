24f13d03843e079aaeba0638604d97cb7a60814d
Piotr Biesek <piotrGTX2@gmail.com>
2018-04-13 19:39:53 +0200

    Conv

--- tensor.py
+++ tensor.py
@@ -6,17 +6,16 @@ X_test, Y_test, _ = loadData("./Test")
 import tflearn
 import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-
-print(X.shape)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 
 # Building Residual Network	
 
-# # 88%
+# # 93%
 net = tflearn.input_data(shape=[None, 401, 11])
 net = tflearn.max_pool_1d(net, 5)
-net = tflearn.dropout(net, 0.7)
-net = tflearn.fully_connected(net, 64, activation='relu')
+net = tflearn.max_pool_1d(net, 4)
+net = tflearn.fully_connected(net, 96, activation='relu')
+net = tflearn.dropout(net, 0.6)
 net = tflearn.fully_connected(net, 14, activation='softmax')
 
 net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy', learning_rate=0.01)
@@ -24,7 +23,7 @@ net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy',
 # Train model
 
 model = tflearn.DNN(net)
-model.fit(X, Y, n_epoch=20, validation_set=(X_test, Y_test), show_metric=True)
+model.fit(X, Y, n_epoch=15, validation_set=(X_test, Y_test), show_metric=True)
 # model.fit(X, Y, n_epoch=12, validation_set=0.08, show_metric=True)
 
 model.save('models/my_model.tflearn')
